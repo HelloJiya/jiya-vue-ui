1fbf4feb138d31aeeae2af9790c6cf72c6422d0e
大宝 <742393511@qq.com>
2021-03-31 17:22:31 +0800

    chore: add element-ui css

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,6 @@
 import IcsUI from '../../src/index.js'
-import "../../packages/theme-chalk/src/index.scss"
 import Element from 'element-ui';
+import "../../packages/theme-chalk/src/index.scss"
 // import 'element-ui/lib/theme-chalk/index.css'
 // import IcsUI from '../../lib/index'
 // import '../../lib/theme-chalk/index.css'
