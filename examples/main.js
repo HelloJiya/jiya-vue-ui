9674de8860a8c64e8560db9b828f8ef6494fd43c
大宝 <742393511@qq.com>
2021-05-13 10:59:46 +0800

    feat: update examples

--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
-import IcsUI from 'main/index.js'
 import App from './index.vue'
+import Element from 'element-ui'
+import IcsUI from 'main/index.js'
 import 'packages/theme-chalk/src/index.scss'
 
+Vue.use(Element)
 Vue.use(IcsUI)
 
 new Vue({// eslint-disable-line
